d6d311bb02be37a50b29a3f67d3574f572918dc6
Diego Santos <diego.santos@olxbr.com>
2018-04-25 08:12:51 -0300

    Add react-redux dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
