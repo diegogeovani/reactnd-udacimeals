e48628062c144a4ce8d22e362d4335b0436b5473
Diego Santos <diego.santos@olxbr.com>
2018-05-07 18:26:06 -0300

    Connect App component to Redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class App extends Component {
 
   render() {
+    console.log('Props: ', this.props)
     return (
       <div>Hello World!</div>
     )
   }
 }
 
-export default App
+function mapStateToProps(calendar) {
+  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: days.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce(
+        (meals, meal) => {
+          meals[meal] = calendar[day][meal] ? calendar[day][meal] : null
+          return meals
+        },
+        {}
+      )
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App)
